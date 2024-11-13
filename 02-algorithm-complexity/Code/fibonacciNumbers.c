564225d96fe93e76cb79fea772a7059015bbaa81
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-11-13 11:55:37 +0300

    Добавь тестов, сократи использование памяти в итеративном алгоритме

--- 02-algorithm-complexity/Code/fibonacciNumbers.c
+++ 02-algorithm-complexity/Code/fibonacciNumbers.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include "headerFile.h"
 #include "time.h"
-#include <assert.h>
 
 long long fibonacciRecursively(int fibonacciIndex) {
     if (fibonacciIndex == 1) {
@@ -15,47 +13,65 @@ long long fibonacciRecursively(int fibonacciIndex) {
     return fibonacciRecursively(fibonacciIndex - 1) + fibonacciRecursively(fibonacciIndex - 2);
 }
 
-long long fibonaccIteratively(int fibonacciIndex) {
-    long long fibonacciNumbers[100] = {0};
-    fibonacciNumbers[1] = 1;
-
-    for (int i = 2; i <= fibonacciIndex; ++i) {
-        fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+long long fibonacciIteratively(int fibonacciIndex) {
+    if (fibonacciIndex < 0) {
+        return -1;
     }
-    return fibonacciNumbers[fibonacciIndex];
+    long long fibonacciNumbers[2] = {1, 0};
+
+    int i = 0;
+    while (i < fibonacciIndex) {
+        long long theLastNumber = fibonacciNumbers[1];
+        fibonacciNumbers[1] = fibonacciNumbers[0] + fibonacciNumbers[1];
+        fibonacciNumbers[0] = theLastNumber;
+        ++i;
+    }
+    return fibonacciNumbers[1];
 }
 
 bool testIterativeAlgorithm() {
-    bool test1Iteratively = fibonaccIteratively(5) == 5;
-    bool test2Iteratively = fibonaccIteratively(30) == 832040;
-    bool test3Iteratively = fibonaccIteratively(60) == 1548008755920;
-    return test1Iteratively && test2Iteratively && test3Iteratively;
+    bool test1Iteratively = fibonacciIteratively(5) == 5;
+    bool test2Iteratively = fibonacciIteratively(30) == 832040;
+    bool test3Iteratively = fibonacciIteratively(60) == 1548008755920;
+    bool test4Iteratively = fibonacciIteratively(0) == 0;
+    bool test5Iteratively = fibonacciIteratively(1) == 1;
+
+
+    return test1Iteratively && test2Iteratively && test3Iteratively && test4Iteratively && test5Iteratively;
 }
 
 bool testRecursiveAlgorithm() {
     bool test1Recursively = fibonacciRecursively(5) == 5;
     bool test2Recursively = fibonacciRecursively(30) == 832040;
-    return test1Recursively && test2Recursively;
+    bool test3Recursively = fibonacciRecursively(0) == 0;
+    bool test4Recursively = fibonacciRecursively(1) == 1;
+
+    return test1Recursively && test2Recursively && test3Recursively && test4Recursively;
 }
 
 void fibonacciNumbersTask() {
-    char strFibonacciIndex[10];
-    char *endptrFibonacciIndex;
     int fibonacciIndex = -1;
 
-    assert(testIterativeAlgorithm());
-    assert(testRecursiveAlgorithm());
+    if (!testIterativeAlgorithm() || !testRecursiveAlgorithm()) {
+        printf("Tests failed\n");
+        return;
+    }
 
     printf("Enter how many times you want to calculate the fibonacci number:\n");
-    scanf("%s", strFibonacciIndex);
-    fibonacciIndex = (int)strtol(strFibonacciIndex, &endptrFibonacciIndex, 10);
-    assert(fibonacciIndex > 0 || *endptrFibonacciIndex == '\0');
+    if (scanf("%d", &fibonacciIndex) != 1) {
+        printf("You didn't enter a number\n");
+        return;
+    }
+    if (fibonacciIndex < 0) {
+        printf("The number must be not negative\n");
+        return;
+    }
 
-    clock_t startFibonaccIteratively = clock();
-    printf("\nIterative algorithm: %lld\n", fibonaccIteratively(fibonacciIndex));
-    clock_t stopFibonaccIteratively = clock();
-    double timeSpentFibonaccIteratively = (double)(stopFibonaccIteratively - startFibonaccIteratively) / CLOCKS_PER_SEC;
-    printf("Time spent: %f seconds\n", timeSpentFibonaccIteratively);
+    clock_t startFibonacciIteratively = clock();
+    printf("\nIterative algorithm: %lld\n", fibonacciIteratively(fibonacciIndex));
+    clock_t stopFibonacciIteratively = clock();
+    double timeSpentFibonacciIteratively = (double)(stopFibonacciIteratively - startFibonacciIteratively) / CLOCKS_PER_SEC;
+    printf("Time spent: %f seconds\n", timeSpentFibonacciIteratively);
 
     clock_t startFibonacciRecursively = clock();
     printf("Recursive algorithm: %lld\n", fibonacciRecursively(fibonacciIndex));
