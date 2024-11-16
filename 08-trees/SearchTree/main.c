bfc5ef67ca3cde64d7362337ca8797a44e9688f5
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-11-16 11:47:56 +0300

    Добавь пользовательский интерфейс

--- 08-trees/SearchTree/main.c
+++ 08-trees/SearchTree/main.c
@@ -1,12 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <locale.h>
 
 #include "../Tree/tree.h"
 
-void printNode(Node* node, bool* errorCode) {
-    NodeValue value = getValue(node, errorCode);
-    printf("'%d': %s\n", value.key, value.value);
+void printTheBackgroundInformation() {
+    printf(
+        "0 – Выход\n"
+        "1 – Добавить значение в словарь\n"
+        "2 – Вывести значение по ключу\n"
+        "3 – Проверить наличие заданного ключа в словаре\n"
+        "4 – Проверить наличие заданного ключа в словаре\n"
+        "5 - Удалить заданный ключ и связанное с ним значение из словаря\n\n"
+    );
+}
+
+void callTheFunction(int functionCode, bool* errorCode) {
+    while (functionCode != 0) {
+        if (functionCode == 1) {
+            printf("Не готово\n");
+        }
+        if (functionCode == 2) {
+            printf("Не готово\n");
+        }
+        if (functionCode == 3) {
+            printf("Не готово\n");
+        }
+        functionCode = getTheFunctionCodeFromTheUser();
+    }
+}
+
+void clearBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+int getTheFunctionCodeFromTheUser(void) {
+    int functionCode = -1;
+    printTheBackgroundInformation();
+    int scanfReturns = scanf("%d", &functionCode);
+    while (functionCode > 5 || functionCode < 0 || scanfReturns != 1) {
+        printf("Введён некорректный номер команды, попробуйте ещё раз:\n");
+        printTheBackgroundInformation();
+        clearBuffer();
+        scanfReturns = scanf("%d", &functionCode);
+    }
+    return functionCode;
 }
 
 int main(void) {
@@ -14,5 +55,10 @@ int main(void) {
     bool errorCode = false;
 
     runTheTreeTests(&errorCode);
+    if (errorCode) {
+        return errorCode;
+    }
+
+    callTheFunction(getTheFunctionCodeFromTheUser(), &errorCode);
     return errorCode;
 }
