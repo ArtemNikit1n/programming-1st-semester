40305983537f5f5ea556fa56ef2fbd07305cbf59
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-11-17 21:43:50 +0300

    добавь комментарии в dictionary.h

--- 08-trees/SearchTree/dictionary.h
+++ 08-trees/SearchTree/dictionary.h
@@ -2,8 +2,11 @@
 
 #include "../Tree/tree.h"
 
+// Adds a node to the binary search tree.
 void addToTheDictionary(Node* node, const int key, const char* value, bool* errorCode);
 
+// Searching for a node in the tree (if the key is not found, NULL will be returned).
 char* findValueByTheKey(Node* node, const int key, bool* errorCode);
 
+// Removes a key from the tree.
 Node* deleteByKey(Node* node, const int key, bool* errorCode);
