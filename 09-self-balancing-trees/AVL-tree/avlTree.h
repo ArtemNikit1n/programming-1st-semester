39364cf71785ee7da06f8df8a370c49f0f5bdac0
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-12-17 16:40:35 +0300

    Update avlTree.h

--- 09-self-balancing-trees/AVL-tree/avlTree.h
+++ 09-self-balancing-trees/AVL-tree/avlTree.h
@@ -1,20 +1,28 @@
 #pragma once
 
+// A structure for storing a value in a node.
 typedef struct NodeValue {
     const char* key;
     const char* value;
 } NodeValue;
 
+// Tree Node.
 typedef struct Node Node;
 
+// Creates a tree.
 Node* createTree(const char* key, const char* value, bool* errorCode);
 
+// Delete a tree.
 void deleteTree(Node** root);
 
+// Searches for a value by key.
 const char* searchByKey(Node* node, const char* key);
 
+// Adds a new value to the tree (if such a key exists, the value for the old key will be deleted).
 Node* addNode(Node* node, const char* key, const char* value, bool* isHeightChanged, bool* errorCode);
 
+// Deletes a node by key.
 Node* deleteNode(Node* node, const char* key, bool* isHeightChanged, bool* errorCode);
 
+// Creates a value that can be stored in the node.
 NodeValue createNodeValue(const char* key, const char* value);
