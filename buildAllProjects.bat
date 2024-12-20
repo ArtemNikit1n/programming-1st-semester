718778c825af7cd68d7fc96095216c584fbb3fbb
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-12-21 01:37:09 +0300

    Добавить почти нормальный путь до .exe

--- buildAllProjects.bat
+++ buildAllProjects.bat
@@ -3,19 +3,25 @@ setlocal EnableDelayedExpansion
 
 set "ScriptDir=%~dp0"
 
-for /f "tokens=1,2 delims==*" %%a in (config.txt) do (
-  set "SolutionName=%%a"
-  set "RelativePath=%%b"
+for /f "delims=" %%a in (config.txt) do (
+  set "RelativePath=%%a"
   set "FullPath=!ScriptDir!!RelativePath!"
+  set "SolutionName=%%~nxa"
 
   echo Processing solution: !SolutionName! (!FullPath!)
 
   if exist "!FullPath!" (
-    msbuild "!FullPath!" /t:Rebuild /p:Configuration=Release /p:Platform=x64 /clp:Summary  
+    msbuild "!FullPath!" /t:Rebuild /p:Configuration=Release /p:Platform=x64 /nologo /clp:ErrorsOnly
     if !errorlevel! neq 0 (
       echo Error building "!SolutionName!".
     ) else (
       echo Successfully built "!SolutionName!"
+       for %%a in ("!ScriptDir!!RelativePath!") do (
+         set "FullPath=%%~dpa"
+       )
+      echo !FullPath!x64\Release\!SolutionName!.exe
+      set "ProgramPath=!FullPath!\Release\x64\!SolutionName!.exe"
+      
     )
   ) else (
     echo Error: Solution file "!FullPath!" not found.
@@ -23,4 +29,3 @@ for /f "tokens=1,2 delims==*" %%a in (config.txt) do (
 )
 
 endlocal
-echo Done. 
